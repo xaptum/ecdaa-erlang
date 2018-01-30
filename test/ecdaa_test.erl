fe636ff9fbb08db9b0bba5f826e53c0a3e49e061
Irina Guberman <irina.guberman@gmail.com>
2018-01-30 17:45:19 -0600

    fix test

--- test/ecdaa_test.erl
+++ test/ecdaa_test.erl
@@ -12,10 +12,11 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -define(MODBYTES_256_56, 32).
--define(SIG_LENGTH, (2*?MODBYTES_256_56 + 5*(2*?MODBYTES_256_56 + 1))).
+-define(SIG_SIZE, (2*?MODBYTES_256_56 + 4*(2*?MODBYTES_256_56 + 1))).
 -define(MESSAGE_BIN, "message.bin").
 -define(CREDENTIAL_BIN, "cred.bin").
 -define(SECRET_KEY_BIN, "sk.bin").
+-define(SIG_BIN, "sig.bin").
 -define(BASENAME_BIN, "basename.bin").
 -define(BASENAME, <<"mybasename">>).
 -define(MESSAGE, <<"Hello ECDAA!">>).
@@ -23,10 +24,11 @@
 
 member_sign_no_basename_test() ->
   Priv = ecdaa:priv_dir(),
+  {ok, Signature} = file:read_file(priv_file(Priv, ?SIG_BIN)),
   Signature = ecdaa:sign(priv_file(Priv, ?MESSAGE_BIN), priv_file(Priv, ?SECRET_KEY_BIN), priv_file(Priv, ?CREDENTIAL_BIN)),
-  io:format("Got signature ~p", [Signature]),
+  io:format("Got signature ~p of size ~b, expecting size ~b~n", [Signature, size(Signature), ?SIG_SIZE]),
   ?assert(is_binary(Signature)),
-  ?assert(size(Signature) == ?SIG_LENGTH),
+  ?assert(size(Signature) =:= ?SIG_SIZE),
 
   %% either filename or binary supported for message field, test it too
   Signature = ecdaa:sign(?MESSAGE, priv_file(Priv, ?SECRET_KEY_BIN), priv_file(Priv, ?CREDENTIAL_BIN)).
@@ -34,10 +36,11 @@ member_sign_no_basename_test() ->
 
 member_sign_with_basename_test() ->
   Priv = ecdaa:priv_dir(),
+  {ok, Signature} = file:read_file(priv_file(Priv, ?SIG_BIN)),
   Signature = ecdaa:sign(priv_file(Priv, ?MESSAGE_BIN), priv_file(Priv, ?SECRET_KEY_BIN), priv_file(Priv, ?CREDENTIAL_BIN), priv_file(Priv, ?BASENAME_BIN)),
-  io:format("Got signature ~p", [Signature]),
+  io:format("Got signature ~p of size ~b, expecting size ~b~n", [Signature, size(Signature), ?SIG_SIZE]),
   ?assert(is_binary(Signature)),
-  ?assert(size(Signature) == ?SIG_LENGTH),
+  ?assert(size(Signature) =:= ?SIG_SIZE),
 
   %% either filename and/or binary supported for message and/or mybasename field, test it too
   Signature = ecdaa:sign(?MESSAGE, priv_file(Priv, ?SECRET_KEY_BIN), priv_file(Priv, ?CREDENTIAL_BIN), ?BASENAME).
